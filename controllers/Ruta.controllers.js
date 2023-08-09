8a682575e4b4f1361a001abb971b10b924332aef
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-08-09 16:55:09 -0500

    feat:

--- controllers/Ruta.controllers.js
+++ controllers/Ruta.controllers.js
@@ -25,7 +25,29 @@ const postRuta = async(req,res) =>{
     res.json({msb: "post ruta",ruta});
 }
 
+const deleteRuta = async(req, res)=>{
+    const {id} = req.params
+
+    const ruta = await Ruta.deleteOne({_id: id});
+
+    res.json(ruta);
+}
+
+const putRutas  = async(req, res)=>{
+    const {id} = req.params;
+    const { _id, ...resto } = req.body;
+
+    const ruta = await Ruta.findByIdAndUpdate( id, resto );
+
+    res.json({
+        msg:"Ruta Actualizado",
+        ruta
+    });
+}
+
 export{
     getRuta,
-    postRuta
+    postRuta,
+    deleteRuta,
+    putRutas
 }
--- routes/ruta.routes.js
+++ routes/ruta.routes.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
 import {check} from 'express-validator';
-import { getRuta, postRuta } from '../controllers/Ruta.controllers.js';
+import { getRuta, postRuta, deleteRuta, putRutas } from '../controllers/Ruta.controllers.js';
 import validateDocuments from '../middlewares/validate.documents.js';
 import { rutaCentroExisteId, rutaExistsById, rutaExiste } from '../helpers/db.validators.js';
+import Ruta from '../models/Ruta.js';
 const router = Router();
 
 router.get('/', getRuta);
@@ -13,5 +14,19 @@ router.post('/', [
     check('centro').custom(rutaCentroExisteId),
     validateDocuments
 ],postRuta);
+
+router.delete('/:id', [
+    check('id').not().isEmpty(),
+    check('id').custom(rutaExistsById)
+],deleteRuta);
+
+router.put('/:id', [
+    check('id').not().isEmpty(),
+    check('id').custom(rutaExistsById),
+    check('nombre').not().isEmpty(),
+    check('centro').not().isEmpty(),
+    check('centro').custom(rutaCentroExisteId),
+    validateDocuments
+],putRutas)
 
 export default router;
